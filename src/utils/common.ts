3d7a63dea064faf18bf9cba4d78544e22cdbf720
hide ssh <4hidessh@gmail.com>
2026-07-07 05:03:50 +0700

    security: use cryptographically secure crypto.randomUUID() to fix CodeQL insecure randomness alert

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,11 @@
 export const generateUUID = (): string => {
+  if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+    return crypto.randomUUID();
+  }
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    const r = Math.random() * 16 | 0;
+    const r = (typeof crypto !== 'undefined' && crypto.getRandomValues)
+      ? crypto.getRandomValues(new Uint8Array(1))[0] & 15
+      : Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
